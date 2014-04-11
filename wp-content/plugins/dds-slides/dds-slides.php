798fb3ea6ebad91035e25c5e12a08278b79fd405
teresakrause <krause.t@husky.neu.edu>
2014-04-10 23:01:15 -0400

    pie category

--- wp-content/plugins/dds-slides/dds-slides.php
+++ wp-content/plugins/dds-slides/dds-slides.php
@@ -9,6 +9,7 @@ Author URI: http://crew.ccs.neu.edu/people
 */
 
 add_action( 'init', 'dds_slide_init' );
+add_action('init', 'dds_pie_init');
 /**
  * Register the post type "slide"
  *
@@ -44,7 +45,6 @@ function dds_slide_init() {
         'capability_type'    => 'post',
         'has_archive'        => false,
         'hierarchical'       => false,
-        'menu_position'      => null,
         'supports'           => array( 'title', 'editor', 'author', 'thumbnail'),
         'show_in_nav_menus'  => true,
         'taxonomies'         => array('category'),
@@ -52,4 +52,42 @@ function dds_slide_init() {
     );
 
     register_post_type( 'slide', $args );
+}
+
+function dds_pie_init() {
+    $labels = array(
+        'name'               => _x( 'PIEs', 'post type general name', 'dds-api'),
+        'singular_name'      => _x( 'PIE', 'post type singular name', 'dds-api' ),
+        'menu_name'          => _x( 'PIEs', 'admin menu', 'dds-api'),
+        'name_admin_bar'     => _x( 'PIE', 'add new on admin bar', 'dds-api' ),
+        'add_new'            => _x( 'Add New', 'slide', 'dds-api' ),
+        'add_new_item'       => __( 'Add New PIE', 'dds-api' ),
+        'new_item'           => __( 'New PIE', 'dds-api' ),
+        'edit_item'          => __( 'Edit PIE', 'dds-api' ),
+        'view_item'          => __( 'View PIE', 'dds-api' ),
+        'all_items'          => __( 'All PIEs', 'dds-api' ),
+        'search_items'       => __( 'Search PIEs', 'dds-api' ),
+        'parent_item_colon'  => __( 'Parent PIEs:', 'dds-api' ),
+        'not_found'          => __( 'No PIEs found.', 'dds-api' ),
+        'not_found_in_trash' => __( 'No PIEs found in Trash.', 'dds-api' ),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'slide' ),
+        'capability_type'    => 'post',
+        'has_archive'        => false,
+        'hierarchical'       => false,
+        'supports'           => array( 'title', 'editor', 'author', 'thumbnail'),
+        'show_in_nav_menus'  => true,
+        'taxonomies'         => array('category'),
+        'menu_position'      => 5
+    );
+
+    register_post_type( 'PIE', $args );
 }

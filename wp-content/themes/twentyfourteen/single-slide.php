380bbaba16baaf2c29470f8471d4c6b486018085
wolfd <2dawolf@gmail.com>
2014-04-13 20:45:47 -0400

    Removed max-width on wordpress theme.

--- wp-content/themes/twentyfourteen/single-slide.php
+++ wp-content/themes/twentyfourteen/single-slide.php
@@ -1,50 +1,49 @@
 <?php
-if (isset($_GET['pie_name'])) { include_once('pie-html.php'); } //show page for PIEs
-else { ?>
+if (isset($_GET['pie_name'])) {
+    include_once('pie-html.php');
+} //show page for PIEs
+else {
+    ?>
 
-<?php
-/**
- * The Template for displaying all single posts
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
+    <?php
+    /**
+     * The Template for displaying all single posts
+     *
+     * @package WordPress
+     * @subpackage Twenty_Fourteen
+     * @since Twenty Fourteen 1.0
+     */
 
-get_header(); ?>
-        <style type="text/css">
-            html, body, #page, #main {
-                height: 100%;
-            }
-        </style>
-        <div id="primary" class="content-area" style="height: 100%;">
-                <div id="content" class="site-content" role="main" style="height: 100%;">
-                        <?php
-                                // Start the Loop.
-                                while ( have_posts() ) : the_post();
- 					echo "<iframe id='pie_display' width='100%' height='100%' src='".esc_url(get_permalink())."&pie_name=demo'></iframe>";
-                                        /*
-                                         * Include the post format-specific template for the content. If you want to
-                                         * use this in a child theme, then include a file called called content-___.php
-                                         * (where ___ is the post format) and that will be used instead.
-                                         */
-                                        //get_template_part( 'content', get_post_format() );
+    get_header(); ?>
+    <style type="text/css">
+        html, body, #page, #main {
+            height: 100%;
+        }
+    </style>
+    <div id="primary" class="content-area" style="height: 100%;">
+        <div id="content" class="site-content" role="main" style="height: 100%;">
+            <?php
+            // Start the Loop.
+            while (have_posts()) : the_post();
+                ?>
+                <span style="margin:0px;padding:0px;overflow:hidden;width:100%;height:100%">
+                    <iframe id='pie_display' src='<?php esc_url(get_permalink()); ?>&pie_name=demo' frameborder="0"
+                            style="overflow:hidden;height:150%;width:150%" height="150%" width="150%"></iframe>
+                </span>
+                <?php
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) {
+                    comments_template();
+                }
+            endwhile;
+            ?>
+        </div>
+        <!-- #content -->
+    </div><!-- #primary -->
 
-                                        // Previous/next post navigation.
-                                        //twentyfourteen_post_nav();
-
-                                        // If comments are open or we have at least one comment, load up the comment template.
-                                        if ( comments_open() || get_comments_number() ) {
-                                                comments_template();
-                                        }
-                                endwhile;
-                        ?>
-                </div><!-- #content -->
-        </div><!-- #primary -->
-
-<?php
-get_sidebar( 'content' );
-get_sidebar();
-get_footer();
+    <?php
+    get_sidebar('content');
+    get_sidebar();
+    get_footer();
 
 } //end if

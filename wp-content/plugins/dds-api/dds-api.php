a1aa5951c94351b29cc2d87c6bccedc18e3e1651
wolfd <2dawolf@gmail.com>
2014-04-14 18:54:16 -0400

    Added external url option. Untested branch. Changed pie_name in api from demo to the pie's actual name. Added get_slide_location. Lack of central helper function file means this function is duplicated in the code. We need to consolidate functionality into fewer plugins. Added external url section to meta box for slide creation. Changed single-slide.php to iframe external urls where necessary.

--- wp-content/plugins/dds-api/dds-api.php
+++ wp-content/plugins/dds-api/dds-api.php
@@ -12,49 +12,48 @@ if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'dds_api') {
     @ini_set('display_errors', 'Off');
 }
 
-add_action( 'wp_ajax_dds_api', 'dds_api_call');
-add_action( 'wp_ajax_nopriv_dds_api', 'dds_api_call');
+add_action('wp_ajax_dds_api', 'dds_api_call');
+add_action('wp_ajax_nopriv_dds_api', 'dds_api_call');
 /**
  * Creates an array of posts and retrieves posts based on the given criteria.
  * To learn more about the get_posts command, check out this link:
  * @link https://codex.wordpress.org/Template_Tags/get_posts
  */
-function dds_api_call() {
+function dds_api_call()
+{
 
 
     if (!isset($_REQUEST['pie_name'])) {
-        wp_send_json(array('errors' => array( array( 'message' => '\'pie_name\' not set' ))));
+        wp_send_json(array('errors' => array(array('message' => '\'pie_name\' not set'))));
     }
     $pie_name = $_REQUEST['pie_name'];
 
 
-
- $args = array(
-        'name'             => $pie_name,  // This is not the post title... this is the post's sanitized title... sooooooo problem
-        'posts_per_page'   => 1,
-        'offset'           => 0,
-        'category'         => '',
-        'orderby'          => 'post_date',
-        'order'            => 'DESC',
-        'include'          => '',
-        'exclude'          => '',
-        'meta_key'         => '',
-        'meta_value'       => '',
-        'post_type'        => 'PIE',
-        'post_mime_type'   => '',
-        'post_parent'      => '',
-        'post_status'      => 'publish',
-	    'suppress_filters' => true
+    $args = array(
+        'name' => $pie_name, // This is not the post title... this is the post's sanitized title... sooooooo problem
+        'posts_per_page' => 1,
+        'offset' => 0,
+        'category' => '',
+        'orderby' => 'post_date',
+        'order' => 'DESC',
+        'include' => '',
+        'exclude' => '',
+        'meta_key' => '',
+        'meta_value' => '',
+        'post_type' => 'PIE',
+        'post_mime_type' => '',
+        'post_parent' => '',
+        'post_status' => 'publish',
+        'suppress_filters' => true
     );
 
     $pie_posts = get_posts($args);
-    
-    if (empty($pie_posts) || count($pie_posts) > 1 ) { 
-	    wp_send_json(array('errors' => array(array('message' => "invalid pie_name $pie_name"))));
-    }
-    
-    $pie_post = $pie_posts[0];
 
+    if (empty($pie_posts) || count($pie_posts) > 1) {
+        wp_send_json(array('errors' => array(array('message' => "invalid pie_name $pie_name"))));
+    }
+
+    $pie_post = $pie_posts[0];
 
 
     $catids = wp_get_post_categories($pie_post->ID);
@@ -64,12 +63,12 @@ function dds_api_call() {
 
     foreach ($catids as $cur_category) {
         $slides = get_posts(array(
-            'posts_per_page'   => -1,
-            'category'         => $cur_category,
-            'orderby'          => 'ID',
-            'order'            => 'DESC',
-            'post_type'        => 'slide',
-            'post_status'      => 'publish',
+            'posts_per_page' => -1,
+            'category' => $cur_category,
+            'orderby' => 'ID',
+            'order' => 'DESC',
+            'post_type' => 'slide',
+            'post_status' => 'publish',
             'suppress_filters' => false
         ));
         foreach ($slides as $slide) {
@@ -79,26 +78,30 @@ function dds_api_call() {
         }
     }
 
-
- /**
-    $myposts = get_posts( $query_args );
-
-    $actions = array();
-    foreach ($myposts as $p) {
-        $actions[] = array('location' => get_permalink($p->ID) . '&pie_name=demo', 'duration' =>(float) get_post_meta($p->ID, 'duration', true));
-    }
-
-    $arr = array('actions' => $actions);
-
-    wp_send_json($arr);
- */
     $actions = array();
     foreach ($posts as $p) {
-        $actions[] = array('location' => get_permalink($p->ID) . '&pie_name=demo', 'duration' =>(float) get_post_meta($p->ID, 'dds_duration', true));
+        $actions[] = array(
+            'location' => get_slide_location($pie_name, $p->ID),
+            'duration' => (float)get_post_meta($p->ID, 'dds_duration', true));
     }
 
     $arr = array('actions' => $actions);
 
     wp_send_json($arr);
+}
+
+/** Generates the location URL for the specified slide post.
+ * @param $pie string the name of the PIE as a string
+ * @param $post_id number the post to get location of
+ * @return string the location URL as a string
+ */
+function get_slide_location($pie, $post_id)
+{
+    $external_url = get_post_meta($post_id, 'dds_external_url', true);
+    if ($external_url && is_string($external_url)) {
+        return $external_url;
+    } else {
+        return get_permalink($post_id) . '&pie_name=' . $pie;
+    }
 }
 
--- wp-content/themes/twentyfourteen/single-slide.php
+++ wp-content/themes/twentyfourteen/single-slide.php
@@ -1,5 +1,6 @@
 <?php
 if (isset($_GET['pie_name'])) {
+    // get styles.
     include_once('pie-html.php');
 } //show page for PIEs
 else {
@@ -33,11 +34,31 @@ else {
     <div id="primary" class="content-area" style="height: 100%; padding-top: 0">
         <div id="content" class="site-content" role="main" style="height: 100%;">
             <?php
+
+            /** Generates the location URL for the specified slide post.
+             * @param $pie string the name of the PIE as a string
+             * @param $post_id number the post to get location of
+             * @return string the location URL as a string
+             */
+            function get_slide_location($pie, $post_id)
+            {
+                $external_url = get_post_meta($post_id, 'dds_external_url', true);
+                if ($external_url && is_string($external_url)) {
+                    return $external_url;
+                } else {
+                    return get_permalink($post_id) . '&pie_name=' . $pie;
+                }
+            }
+
             // Start the Loop.
             while (have_posts()) : the_post();
+
+
+
                 ?>
                 <span style="margin:0px;padding:0px;overflow:hidden;width:100%;height:100%">
-                    <iframe id='pie_display' src='<?php echo esc_url(get_permalink()); ?>&pie_name=demo' frameborder="0"
+                    <iframe id='pie_display' src='<?php echo esc_url(get_slide_location("demo", get_the_ID())); ?>'
+                            frameborder="0"
                             style="overflow:hidden;height:100%;width:150%" height="100%" width="150%" seamless></iframe>
                 </span>
                 <?php

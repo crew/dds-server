8abb55880049c2f319c627f9f1c2def8aed7334f
teresakrause <krause.t@husky.neu.edu>
2014-05-25 15:33:46 -0400

    no people in trash

--- wp-content/plugins/dds-nfc/dds-nfc.php
+++ wp-content/plugins/dds-nfc/dds-nfc.php
@@ -1,22 +1,22 @@
 /**
- * Register the post type "slide"
+ * Register the post type "person"
  *
  * @link http://codex.wordpress.org/Function_Reference/register_post_type
  */
 function dds_person_init() {
   $labels = array(
-    'name'               => _x( 'Slides', 'post type general name', 'dds-people' ),
-    'singular_name'      => _x( 'Slide', 'post type singular name', 'dds-people' ),
-    'menu_name'          => _x( 'Slides', 'admin menu', 'dds-people' ),
-    'name_admin_bar'     => _x( 'Slide', 'add new on admin bar', 'dds-people' ),
+    'name'               => _x( 'People', 'post type general name', 'dds-people' ),
+    'singular_name'      => _x( 'Person', 'post type singular name', 'dds-people' ),
+    'menu_name'          => _x( 'People', 'admin menu', 'dds-people' ),
+    'name_admin_bar'     => _x( 'Person', 'add new on admin bar', 'dds-people' ),
     'add_new'            => _x( 'Add New', 'person', 'dds-people' ),
-    'add_new_item'       => __( 'Add New Slide', 'dds-people' ),
-    'new_item'           => __( 'New Slide', 'dds-people' ),
-    'edit_item'          => __( 'Edit Slide', 'ddspeople-people' ),
-    'view_item'          => __( 'View Slide', 'dds-people' ),
-    'all_items'          => __( 'All Slides', 'dds-people' ),
-    'search_items'       => __( 'Search Slides', 'dds-people' ),
-    'parent_item_colon'  => __( 'Parent Slides:', 'dds-people' ),
+    'add_new_item'       => __( 'Add New Person', 'dds-people' ),
+    'new_item'           => __( 'New Person', 'dds-people' ),
+    'edit_item'          => __( 'Edit Person', 'dds-people' ),
+    'view_item'          => __( 'View Person', 'dds-people' ),
+    'all_items'          => __( 'All People', 'dds-people' ),
+    'search_items'       => __( 'Search People', 'dds-people' ),
+    'parent_item_colon'  => __( 'Parent People:', 'dds-people' ),
     'not_found'          => __( 'No people found.', 'dds-people' ),
     'not_found_in_trash' => __( 'No people found in Trash.', 'dds-people' ),
   );
@@ -28,14 +28,19 @@ function dds_person_init() {
     'show_ui'            => true,
     'show_in_menu'       => true,
     'query_var'          => true,
-    'rewrite'            => array( 'slug' => 'slide' ),
+    'rewrite'            => array( 'slug' => 'person' ),
     'capability_type'    => 'post',
     'has_archive'        => false,
     'hierarchical'       => false,
-    'supports'           => array( 'title', 'editor', 'author', 'thumbnail' ),
-    'show_in_nav_menus'  => true,
-    'taxonomies'         => array( 'category' ),
-    'menu_position'      => 5
+    'supports'           => array( 'custom fields'),
+    'show_in_nav_menus'  => false,
+    'menu_position'      => 5,
+    'capabilities'       => array(
+                  "manage_options",
+                  "read_post",
+                  "manage_options",
+                  "manage_options",
+                  "manage_options"),
   );
 
   register_post_type( 'slide', $args );
